8c499eb22dfdfbce7473588b4bbdf3bb380785e6
bkklc <abkklc@gmail.com>
2023-11-05 23:53:16 +0300

    MyDictionaryList

---
+++ MyDictionaryList/DictionaryList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionaryList
+{
+    public class DictionaryList<Key, Value>
+    {
+        
+        Key[] keys;
+        Value[] values;
+
+        // constructor - yapıcı metot
+        public DictionaryList()
+        {
+            
+            keys = new Key[0]; // 0 elemalı bir array oluşturuluyor
+            values = new Value[0]; // 0 elemalı bir array oluşturuluyor
+        }
+
+        public void Add(Key key, Value value)
+        {
+            Key[] tempKeys = keys; // keys önceki değerlerini (referansını) tempKeys tutuyoruz.
+            Value[] tempValues = values; // values önceki değerlerini (referansını) tempValues tutuyoruz.
+
+            keys = new Key[keys.Length + 1]; // keysin eleman sayısını artırıyoruz.
+            values = new Value[values.Length + 1]; // valuesin eleman sayısını artırıyoruz.
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+                
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;                     
+        }
+
+        public int Length
+        {
+            get { return keys.Length; }
+
+        }
+
+        public Key[] _Keys
+        {
+            get { return keys; }
+        }
+
+        public Value[] _Values
+        {
+            get { return values; }
+        }
+
+
+
+    }
+}
--- MyDictionaryList/Program.cs
+++ MyDictionaryList/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MyDictionaryList;
+
+
+
+DictionaryList<int,string> dictionaryList = new DictionaryList<int, string>();
+dictionaryList.Add( 1 ,"Ahmet");
+dictionaryList.Add( 2 ,"Berk");
+
+
+for (int i = 0; i < dictionaryList.Length; i++)
+{
+    Console.WriteLine( dictionaryList._Keys[i] + " " + dictionaryList._Values[i]);
+
+}
